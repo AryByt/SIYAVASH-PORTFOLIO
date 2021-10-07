8ec1ba9de3c3f4f7e981a93fe9ee945972657caf
arybyt <bayatiarya@gmail.com>
2021-10-06 21:49:57 -0500

    navbar added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,20 +2,19 @@ import { Link } from 'react-router-dom';
 
 const Nav = ({ user }) => {
   return (
-    <div className="meImg">
+    <div className="flex justify-around bg-green-600 text-white text-bold  font-mono ">
       <div>
         <nav>
           <div className="nav-bar">
-            <Link className="link" to="/">
+            <Link className="m-32 hover:bg-white hover:text-gray-900" to="/">
               About
             </Link>
-            <Link className="link" to="/projects">
+            <Link className="m-32 hover:bg-white hover:text-gray-900" to="/projects">
               Projects
             </Link>
-            <Link className="link" to="/resume">
+            <Link className="m-32 hover:bg-white hover:text-gray-900 " to="/resume">
               Resume
             </Link>
-            <div className="links"></div>
           </div>
         </nav>
       </div>
